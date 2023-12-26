78c83676df69a5f894bf8c1761657ac69b540c70
Nirav Patel <64038962+Niravanaa@users.noreply.github.com>
2023-12-25 23:03:18 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.glovoapp.android-versioning") version "0.1.24"
+    id("com.glovoapp.semantic-versioning") version "0.1.24"
 }
