e372de97ca280b1c85b3afcf9c228c5c4274a05f
Nirav Patel <64038962+Niravanaa@users.noreply.github.com>
2023-12-26 23:46:37 -0500

    Delete increment_version.sh

--- increment_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "${GITHUB_EVENT_NAME}" == "push" && $(git log -1 --pretty=%B) == *"[MAJOR]"* ]]; then
-  ./gradlew incrementSemanticVersion --major
-elif [[ "${GITHUB_EVENT_NAME}" == "push" && $(git log -1 --pretty=%B) == *"[MINOR]"* ]]; then
-  ./gradlew incrementSemanticVersion --minor
-else
-  ./gradlew incrementSemanticVersion --patch
-fi
